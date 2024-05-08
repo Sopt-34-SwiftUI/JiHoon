bbdb40e33f99d36046e0186a473be6f9fe141910
hooni <jhlee909090@gmail.com>
2024-05-08 10:48:32 +0900

    git 연결

---
+++ swiftUIStudy/swiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIStudyApp.swift
+//  swiftUIStudy
+//
+//  Created by 이지훈 on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
