465b058bd8440e9576d8f0fb7ed6c1392731f5fb
hooni <jhlee909090@gmail.com>
2024-05-10 18:04:07 +0900

    기본 계산기 버튼 UI 구현

--- swiftUIStudy/CalculatorMF.swift
+++ swiftUIStudy/CalculatorMF.swift
@@ -1,28 +1,28 @@
-////
-////  CalculatorMF.swift
-////  swiftUIStudy
-////
-////  Created by 이지훈 on 5/10/24.
-////
 //
-//import Foundation
-//import SwiftUI
+//  CalculatorMF.swift
+//  swiftUIStudy
 //
-//struct CalculatorButtonModifier: ViewModifier {
-//    
-//    func body(content: Content) -> some View {
-//        content
-//            .frame(width: 80, height: 80)
-//            .background(Color.gray)
-//            .cornerRadius(60)
-//            .foregroundColor(.white)
-//            .font(.system(size: 40))
-//    }
-//    
-//}
+//  Created by 이지훈 on 5/10/24.
 //
-//extension View {
-//    func calculatorButtonStyle() -> some View {
-//        self.modifier(CalculatorButtonModifier())
-//    }
-//}
+
+import Foundation
+import SwiftUI
+
+struct CalculatorButtonModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 80, height: 80)
+            .background(Color.gray)
+            .cornerRadius(60)
+            .foregroundColor(.white)
+            .font(.system(size: 40))
+    }
+    
+}
+
+extension View {
+    func calculatorButtonStyle() -> some View {
+        self.modifier(CalculatorButtonModifier())
+    }
+}
