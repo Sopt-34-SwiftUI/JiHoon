7bdad757c5efbc54f3720884438a45a5b0cdf6cf
hooni <jhlee909090@gmail.com>
2024-05-10 18:03:36 +0900

    브랜치 생성

--- swiftUIStudy/CalculatorMF.swift
+++ swiftUIStudy/CalculatorMF.swift
@@ -1,28 +1,28 @@
+////
+////  CalculatorMF.swift
+////  swiftUIStudy
+////
+////  Created by 이지훈 on 5/10/24.
+////
 //
-//  CalculatorMF.swift
-//  swiftUIStudy
+//import Foundation
+//import SwiftUI
 //
-//  Created by 이지훈 on 5/10/24.
+//struct CalculatorButtonModifier: ViewModifier {
+//    
+//    func body(content: Content) -> some View {
+//        content
+//            .frame(width: 80, height: 80)
+//            .background(Color.gray)
+//            .cornerRadius(60)
+//            .foregroundColor(.white)
+//            .font(.system(size: 40))
+//    }
+//    
+//}
 //
-
-import Foundation
-import SwiftUI
-
-struct CalculatorButtonModifier: ViewModifier {
-    
-    func body(content: Content) -> some View {
-        content
-            .frame(width: 80, height: 80)
-            .background(Color.gray)
-            .cornerRadius(60)
-            .foregroundColor(.white)
-            .font(.system(size: 40))
-    }
-    
-}
-
-extension View {
-    func calculatorButtonStyle() -> some View {
-        self.modifier(CalculatorButtonModifier())
-    }
-}
+//extension View {
+//    func calculatorButtonStyle() -> some View {
+//        self.modifier(CalculatorButtonModifier())
+//    }
+//}
