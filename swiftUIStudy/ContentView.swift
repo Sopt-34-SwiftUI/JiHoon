2a8fe37c2d3319d05a367e0dce3a12521c268100
hooni <jhlee909090@gmail.com>
2024-05-25 02:35:37 +0900

    fix- content 추가

--- swiftUIStudy/ContentView.swift
+++ swiftUIStudy/ContentView.swift
@@ -10,6 +10,53 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                HStack{
+                    Text("C").EtcButtonStyle()
+                    Text("+").EtcButtonStyle()
+                    Text("%").EtcButtonStyle()
+                    Text("÷").OperationButtonStyle()
+                }
+                HStack{
+                    Text("7").calculatorButtonStyle()
+                    Text("8").calculatorButtonStyle()
+                    Text("9").calculatorButtonStyle()
+                    Text("x").OperationButtonStyle()
+                }
+                HStack{
+                    Text("4").calculatorButtonStyle()
+                    Text("5").calculatorButtonStyle()
+                    Text("6").calculatorButtonStyle()
+                    Text("-").OperationButtonStyle()
+                }
+                HStack{
+                    Text("1").calculatorButtonStyle()
+                    Text("2").calculatorButtonStyle()
+                    Text("3").calculatorButtonStyle()
+                    Text("+").OperationButtonStyle()
+                }
+                HStack{
+                    Text("0")
+                        .frame(width: 160, height: 80)
+                        .background(Color.gray.opacity(0.5))
+                        .cornerRadius(60)
+                        .foregroundColor(.white)
+                        .font(.system(size: 40))
+                        .frame(maxWidth: .infinity)
+                        .padding(.leading, 10)
+                    Text(".").calculatorButtonStyle()
+                    Text("=").OperationButtonStyle()
+                }
+            }
+            .padding()
+            .background(.black)
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
