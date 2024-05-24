d0662ba91011301fc47fcb321a7b32d7894f14ee
hooni <jhlee909090@gmail.com>
2024-05-25 02:34:39 +0900

    fix: 코드 수정한 부분 안보이는 이슈 수정 - etcMF 삭제

--- swiftUIStudy/EtcMF.swift
+++ swiftUIStudy/EtcMF.swift
@@ -4,24 +4,3 @@
 //
 //  Created by 이지훈 on 5/25/24.
 //
-
-import SwiftUI
-
-struct EtcMF: ViewModifier {
-    
-    func body(content: Content) -> some View {
-        content
-            .frame(width: 80, height: 80)
-            .background(Color.gray)
-            .cornerRadius(60)
-            .foregroundColor(.white)
-            .font(.system(size: 40))
-    }
-    
-}
-
-extension View {
-    func EtcButtonStyle() -> some View {
-        self.modifier(EtcMF())
-    }
-}
