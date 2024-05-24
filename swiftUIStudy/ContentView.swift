4b7c5188d3c38c26bea8c454f951868f846289e5
hooni <jhlee909090@gmail.com>
2024-05-25 02:27:43 +0900

    style 계산기 각 버튼 구현 완료

--- swiftUIStudy/ContentView.swift
+++ swiftUIStudy/ContentView.swift
@@ -1,45 +1,7 @@
-////
-////  ContentView.swift
-////  swiftUIStudy
-////
-////  Created by 이지훈 on 5/5/24.
-////
-//import SwiftUI
 //
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            HStack{
-//                Text("7").calculatorButtonStyle()
-//                Text("8").calculatorButtonStyle()
-//                Text("9").calculatorButtonStyle()
-//                Text("X").OperationButtonStyle()
-//            }
-//            HStack{
-//                Text("4").calculatorButtonStyle()
-//                Text("5").calculatorButtonStyle()
-//                Text("6").calculatorButtonStyle()
-//                Text("-").OperationButtonStyle()
-//            }
-//            HStack{
-//                Text("1").calculatorButtonStyle()
-//                Text("2").calculatorButtonStyle()
-//                Text("3").calculatorButtonStyle()
-//                Text("+").OperationButtonStyle()
-//            }
-//            HStack{
-//                Text("0").calculatorButtonStyle()
-//                Text(".").calculatorButtonStyle()
-//                Spacer()
-//                Text("=").OperationButtonStyle()
-//            }
-//        }
-//        .padding()
-//        .background(.black)
-//    }
-//}
+//  ContentView.swift
+//  swiftUIStudy
 //
-<<<<<<< HEAD
 //  Created by 이지훈 on 5/5/24.
 //
 
@@ -102,10 +64,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-=======
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
->>>>>>> parent of 465b058 (기본 계산기 버튼 UI 구현)
